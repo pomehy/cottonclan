dd394d21b2ccba9fc910416e901a661cc8081987
pomehy <hlopkov.gleb@mail.ru>
2020-06-18 20:22:30 +0300

    скрипт без чекбокса

--- js/health.js
+++ js/health.js
@@ -1,54 +1,49 @@
-let healthLevel = document.querySelector('.level__input');
+let levelInput = document.querySelector('.level__input');
 let levelAlert = document.querySelector('.level__alert');
 
-let healthStamina = document.querySelector('.stamina__input');
+let staminaInput = document.querySelector('.stamina__input');
 let staminaAlert = document.querySelector('.stamina__alert');
+
+let bonusInput = document.querySelector('.bonus__input'); 
 
 let healthButton = document.querySelector('.health__button');
 let healthResult = document.querySelector('.health__result');
 
 
 
-healthButton.onclick = function(evt) {
-    evt.preventDefault();  
-
-    if (healthStamina.value < 0 || healthLevel.value < 0 || healthStamina.value == '' || healthLevel.value == '') {
-        healthResult.classList.remove('health__text--success');
-        healthResult.classList.add('health__text--warning');
-        healthResult.textContent = " Укажите положительное число больше 10";
-
-    } else {
-        healthResult.textContent = ((parseInt(healthStamina.value,10) * parseInt(4,10)) * (parseInt(healthLevel.value,10) + parseInt(1,10)));  
-       
-        healthResult.classList.add('health__text--success');
-    }   
-};
-
-healthLevel.oninput = function() {
-    if (healthLevel.value <= 0 || healthLevel.value == '') {
-        healthResult.classList.remove('health__text--success');
-        healthResult.classList.add('health__text--warning');
+levelInput.oninput = function() {    
+    if (levelInput.value <= 0 || levelInput.value == '') {
+        
         levelAlert.classList.remove('alert-hidden');
         healthResult.textContent = " Укажите верные данные";
-
-
-    } else if (healthLevel.value > 0) {
+        
+    } else if (levelInput.value > 0) {
         levelAlert.classList.add('alert-hidden');    
-        healthResult.textContent = ((parseInt(healthStamina.value,10) * parseInt(4,10)) * (parseInt(healthLevel.value,10) + parseInt(1,10)));         
-        healthResult.classList.add('health__text--success');
-    } 
+        healthResult.textContent = ((parseInt(staminaInput.value,10) * parseInt(4,10)) * (parseInt(levelInput.value,10) + parseInt(1,10)));    
+        
+    } else if (levelInput.value > 0 && bonusInput.checked) {
+        levelAlert.classList.add('alert-hidden');    
+        healthResult.textContent = ((parseInt(staminaInput.value,10) * parseInt(4,10)) * (parseInt(levelInput.value,10) + parseInt(1,10)));
+
+        let resultValue = healthResult.textContent; 
+    }  
      
 };
 
-healthStamina.oninput = function() {
-    if (healthStamina.value < 10 || healthStamina.value == '') {
-        healthResult.classList.remove('health__text--success');
-        healthResult.classList.add('health__text--warning');
+staminaInput.oninput = function() {
+    if (staminaInput.value < 10 || staminaInput.value == '') {        
         staminaAlert.classList.remove('alert-hidden');
         healthResult.textContent = " Укажите верные данные";
-    } else if (healthStamina.value > 9) {
+    } 
+    else if (staminaInput.value > 9) {
         staminaAlert.classList.add('alert-hidden');    
-        healthResult.textContent = ((parseInt(healthStamina.value,10) * parseInt(4,10)) * (parseInt(healthLevel.value,10) + parseInt(1,10)));       
-        healthResult.classList.add('health__text--success');
-    }      
+        healthResult.textContent = ((parseInt(staminaInput.value,10) * parseInt(4,10)) * (parseInt(levelInput.value,10) + parseInt(1,10))); 
+    }  
+    else if (staminaInput.value > 9) {
+        staminaAlert.classList.add('alert-hidden');    
+        healthResult.textContent = ((parseInt(staminaInput.value,10) * parseInt(4,10)) * (parseInt(levelInput.value,10) + parseInt(1,10)));      
+        
+    } 
+      
 };
+
