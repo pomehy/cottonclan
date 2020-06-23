dcb0c3af8179606b1dddefe79ee2d6209e0dd940
pomehy <hlopkov.gleb@mail.ru>
2020-06-23 22:31:06 +0300

    add toggle(fury__icon--boom)

--- js/fury.js
+++ js/fury.js
@@ -2,6 +2,8 @@ let furyLevel = document.querySelector('.fury-level__input');
 let furyChance = document.querySelector('.fury-chance__input');
 let furyResult = document.querySelector('.fury__result');
 
+let furyIcon = document.querySelector('.fury__icon');
+
 
 
 furyLevel.oninput = function() { 
@@ -11,8 +13,12 @@ furyLevel.oninput = function() {
         furyLevel.value = 80;
     } else  {
         furyResult.textContent =  (parseInt(furyLevel.value,10) * (parseInt(furyChance.value,10) - parseInt(30,10))); 
+        
     }
+    furyIcon.classList.toggle('fury__icon--boom')
+    
 }
+
 
 furyChance.oninput = function() {     
     if  (furyChance.value > 70 ) {
@@ -20,5 +26,5 @@ furyChance.oninput = function() {
     } else if (furyChance.value > 50) {
         furyResult.textContent =  (parseInt(furyLevel.value,10) * (parseInt(furyChance.value,10) - parseInt(30,10))); 
     }
-
+    furyIcon.classList.toggle('fury__icon--boom')
 }
