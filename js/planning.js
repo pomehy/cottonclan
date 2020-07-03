9afbdde026cbb8aa53ee0bd6599c4878a3967e32
pomehy <hlopkov.gleb@mail.ru>
2020-07-03 11:12:23 +0300

    add planning.js

---
+++ js/planning.js
@@ -0,0 +1,23 @@
+let planningBase = document.querySelector('.planning__base');
+let planningTotal = document.querySelector('.planning__total');
+let planningResult = document.querySelector('.planning__result');
+
+let planningButton = document.querySelector('.planning__button');
+
+
+
+
+planningButton.onclick = function(evt) {
+    evt.preventDefault();
+    let valueBase = parseInt(planningBase.value,10) + parseInt(1,10);       
+    let priceBase =  Math.ceil(0.65 * (valueBase - parseInt(10,10)) ** 1.5); 
+    let priceResult = 0;
+    while (valueBase <= (parseInt(planningTotal.value,10))) { 
+        priceResult = parseInt(priceResult,10) + (Math.ceil(0.65 * (valueBase - parseInt(10,10)) ** 1.5));
+        valueBase++;
+    }
+    planningResult.textContent = priceResult.toLocaleString();
+   
+};
+
+ 
