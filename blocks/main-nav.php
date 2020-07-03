5e51b93d6ca196e3798a4a0f05161af989f30287
pomehy <hlopkov.gleb@mail.ru>
2020-07-03 11:11:20 +0300

    add planning.php index.php

--- blocks/main-nav.php
+++ blocks/main-nav.php
@@ -1,11 +1,8 @@
 <nav class="main-nav">
-    <ul class="main-nav__list">
-        <!-- <li class="main-nav__item">
-            <a href="character.html" class="main-nav__link">Персонаж</a>
-        </li> -->
-        <!-- <li class="main-nav__item submenu">
-            <a href="accuracy.html" class="main-nav__link">Меткость</a>
-        </li> -->
+    <ul class="main-nav__list"> 
+    <li class="main-nav__item">
+            <a href="index.php" class="main-nav__link">Главная</a>
+        </li>       
         <li class="main-nav__item">
             <a href="health.php" class="main-nav__link">Выносливость</a>
         </li>
@@ -14,15 +11,12 @@
         </li>
         <li class="main-nav__item">
             <a href="weapon.php" class="main-nav__link">Владение оружием</a>
-        </li>
-        <!-- <li class="main-nav__item">
-            <a href="bullets.html" class="main-nav__link">Пули</a>
-        </li>
+        </li>        
         <li class="main-nav__item">
-            <a href="gold.html" class="main-nav__link">Жетоны</a>
-        </li> -->
+            <a href="planning.php" class="main-nav__link">Стоимость характеристик</a>
+        </li>
         <li class="main-nav__item">
             <a href="pet.php" class="main-nav__link">Питомец</a>
-        </li>
+        </li>        
     </ul>
 </nav>
