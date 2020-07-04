ba75b3e7f6a3ad69d47b5a497757b707f672441c
pomehy <hlopkov.gleb@mail.ru>
2020-07-04 14:07:45 +0300

    add page aboutclan.php

---
+++ aboutclan.php
@@ -0,0 +1,35 @@
+<?php require 'blocks/head.php' ?>
+<link rel="stylesheet" href="css/aboutclan.css">
+<title>О клане CottonClan</title>
+</head>
+<?php require 'blocks/header.php' ?>
+<main class="page-main container">
+    <?php require 'blocks/main-nav.php' ?>
+    <section class="main-content aboutclan">
+        <div class="row">
+            <section class="col-lg-5 main-content__decription">
+                <h2 class="main-content__title">CottonClan</h2>
+                <p class="main-content__text">CottonClan - клан(коллектив игроков) по игровому приложению Метро 2033 !</p>
+                <p class="main-content__text">Клан был основан 03.01.2017 г.</p>
+                <p class="main-content__text">Клан основан на принципах взаимопонимания, честности, открытости и поддержки !</p>
+                <p class="main-content__text">Глава и основатель клана <a href="https://vk.com/hlopkov_gleb"> Cotton G</a></p>
+                <p class="main-content__highlight">Принимаем в наши ряды активныих игроков ! Писать командиру  <a href="https://vk.com/hlopkov_gleb"> Cotton G</a> </p>
+            </section>
+            <section class="col-lg-7 main-content__form calculate">                
+                <div class="calculate-inner">
+                    <h2 class="calculate__title">Вступить в клан</h2>
+                    <img class="calculate__image" src="/img/aboutclan/clan-screen.png" alt="">
+                    <a href="https://vk.com/im?sel=33134183" target="_blank" class="calculate__button aboutclan-button" type="submit">Написать командиру</a>
+                    
+                    <p class="calculate__important">Принимаем бойцов со статами от 1000 ! Бывают исключения (пишите) !</p>
+                </div>
+            </section>
+        </div>
+    </section>
+</main>
+
+<?php require 'blocks/footer.php' ?>
+
+</body>
+
+</html>
