dca726e278b72a2fbd6664568f576eeef893b24e
pomehy <hlopkov.gleb@mail.ru>
2020-07-05 20:54:59 +0300

    add link main-nav.css

--- blocks/head.php
+++ blocks/head.php
@@ -4,6 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;800&display=swap" rel="stylesheet">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Помощник по игре Метро 2033(Приложение ВК). Калькулятор стоимости, расчёт ярости, здоровья, лайфхаки ! Собтственность CottonClan !"> 
+    <meta name="Keywords" content="Помощник по игре Метро 2033(Приложение ВК). ЛайфХаки, помощь по игре !">
+    <meta name="robots" content="all">     
     <link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" href="css/bootstrap-grid.css">
+    <link rel="stylesheet" href="css/main-nav.css">
     <link rel="stylesheet" href="css/main.css">
