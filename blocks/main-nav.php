afb544a80ac42070900cbd7276442df455688e4f
pomehy <hlopkov.gleb@mail.ru>
2020-07-05 20:54:31 +0300

    add dropdown sub-menu

--- blocks/main-nav.php
+++ blocks/main-nav.php
@@ -5,23 +5,27 @@
         </li>     
         <li class="main-nav__item">
             <a class="main-nav__link main-nav__link--important" href="aboutclan.php">Клан CottonClan</a>
-        </li>
-        <li class="main-nav__item">
-            <a class="main-nav__link" href="planning.php">Стоимость характеристик</a>
-        </li> 
-        <li class="main-nav__item">
-            <a class="main-nav__link" href="health.php">Выносливость</a>
-        </li> 
-        <li class="main-nav__item">
-            <a class="main-nav__link" href="fury.php">Ярость</a>
-        </li> 
-        <li class="main-nav__item">
-            <a class="main-nav__link" href="weapon.php">Урон персонажа</a>
-        </li> 
-        <li class="main-nav__item">
-            <a class="main-nav__link" href="pet.php">Питомец</a>
         </li>   
-        
+        <li class="main-nav__item sub-menu">
+            <a class="main-nav__link" href="calculation.php">Калькуляторы</a>
+            <ul class="sub-menu__list">
+                <li class="sub-menu__item">
+                    <a class="sub-menu__link" href="planning.php">Стоимость характеристик</a>
+                </li>
+                <li class="sub-menu__item">
+                    <a class="sub-menu__link" href="health.php">Выносливость</a>
+                </li>
+                <li class="sub-menu__item">
+                    <a class="sub-menu__link" href="fury.php">Ярость</a>
+                </li>
+                <li class="sub-menu__item">
+                    <a class="sub-menu__link" href="weapon.php">Урон персонажа</a>
+                </li>
+                <li class="sub-menu__item">
+                    <a class="sub-menu__link" href="pet.php">Питомец</a>
+                </li>
+            </ul>
+        </li>        
         <li class="main-nav__item">
             <a class="main-nav__link" href="programs.php">Программы</a>
         </li>       
