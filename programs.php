0e73e5e1067136a0949ae72adec38302ede2bd13
pomehy <hlopkov.gleb@mail.ru>
2020-07-05 20:39:54 +0300

    add programs.php

---
+++ programs.php
@@ -0,0 +1,35 @@
+<?php require 'blocks/head.php' ?>
+<link rel="stylesheet" href="css/programs.css">
+
+<title>Помощник по игре Метро 2033 (приложение ВК) - CottonClan</title>
+</head>
+<?php require 'blocks/header.php' ?>
+<main class="page-main container">
+    <?php require 'blocks/main-nav.php' ?>
+    <section class="main-content">
+        <h2 class="main-content__title">Полезные программы</h2>
+        <p class="main-content__text">В помощь игроку, сделать скриншот, сделать GIF-анимацию ! </p>
+        <div class="row programs">
+            <section class="col-lg-6 programs-wrapper">
+                <div class="programs-inner">
+                    <h2 class="programs__title">Сделать скриншот</h2>
+                    <img class="programs__image" src="img/programs/lightshot-logo.png" alt=""  height="105">
+                    <a href="https://app.prntscr.com/ru/index.html" target="_blank" class="programs__button">перейти на сайт LightShot</a>
+                </div>                               
+            </section>
+            <section class="col-lg-6 programs-wrapper">               
+                <div class="programs-inner">
+                    <h2 class="programs__title">Сделать Gif-Анимацию</h2>
+                    <img class="programs__image" src="img/programs/gifcam-logo.png" alt="" height="105">
+                    <a href="files/gifcam.rar" download class="programs__button">скачать GIFCAM</a>
+                </div>                
+            </section>
+        </div>
+    </section>
+</main>
+
+<?php require 'blocks/footer.php' ?>
+
+</body>
+
+</html>
