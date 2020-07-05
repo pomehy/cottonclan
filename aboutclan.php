1c2071cc342661492606976a9946096b6dc2e973
pomehy <hlopkov.gleb@mail.ru>
2020-07-05 20:27:12 +0300

    fix text aboutclan

--- aboutclan.php
+++ aboutclan.php
@@ -8,20 +8,21 @@
     <section class="main-content aboutclan">
         <div class="row">
             <section class="col-lg-5 main-content__decription">
-                <h2 class="main-content__title">CottonClan</h2>
+                <h2 class="main-content__title">Клан "CottonClan"</h2>
                 <p class="main-content__text">CottonClan - клан(коллектив игроков) по игровому приложению Метро 2033 !</p>
                 <p class="main-content__text">Клан был основан 03.01.2017 г.</p>
                 <p class="main-content__text">Клан основан на принципах взаимопонимания, честности, открытости и поддержки !</p>
                 <p class="main-content__text">Глава и основатель клана <a href="https://vk.com/hlopkov_gleb"> Cotton G</a></p>
-                <p class="main-content__highlight">Принимаем в наши ряды активныих игроков ! Писать командиру  <a href="https://vk.com/hlopkov_gleb"> Cotton G</a> </p>
+                <p class="main-content__text"><b>Принимаем в наши ряды активныих игроков !</b></p>
+                <p class="main-content__highlight">Писать командиру  <a href="https://vk.com/hlopkov_gleb"> Cotton G</a>, к сообщению прикреплять "скриншот" вашего персонажа ! </p>
             </section>
             <section class="col-lg-7 main-content__form calculate">                
                 <div class="calculate-inner">
                     <h2 class="calculate__title">Вступить в клан</h2>
-                    <img class="calculate__image" src="/img/aboutclan/clan-screen.png" alt="">
+                    <img class="calculate__image" src="img/aboutclan/clan-screen.png" alt="">
                     <a href="https://vk.com/im?sel=33134183" target="_blank" class="calculate__button aboutclan-button" type="submit">Написать командиру</a>
                     
-                    <p class="calculate__important">Принимаем бойцов со статами от 1000 ! Бывают исключения (пишите) !</p>
+                    <p class="calculate__important">Принимаем бойцов со статами от 900 ! Бывают исключения (пишите) !</p>
                 </div>
             </section>
         </div>
